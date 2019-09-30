c3813ef41689cce118952f0941d5390d3325611a
Andres Felipe Montealegre Olaya <afmontealegreol@unal.loc>
2019-09-30 17:38:40 -0500

    wrote iterated names and ids on consent TODO: Timer in additions, test bots

--- app_0_consent/tests.py
+++ app_0_consent/tests.py
@@ -2,10 +2,14 @@ from otree.api import Currency as c, currency_range
 from . import pages
 from ._builtin import Bot
 from .models import Constants
+import random, itertools
 
 
 class PlayerBot(Bot):
+    names = itertools.cycle(['Alberto', 'Bolivar', 'Carlos', 'Danilo'])
+    ids = itertools.cycle([33, 44, 55, 66])
 
     def play_round(self):
         yield (pages.Bienvenido)
-        yield (pages.Consent, {'nombre': 'Ramiro Wolf', 'id_number': '999'})
+        yield (pages.Consent, {'nombre': next(self.names), 'id_number': next(self.ids)})
+
