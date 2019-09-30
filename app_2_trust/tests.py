181b6683aa5753413f436e33638991b34f72597a
Andres Felipe Montealegre Olaya <afmontealegreol@unal.loc>
2019-09-30 16:41:33 -0500

    wrote random bots apparently correct. TODO: Timer in additions, test bots

--- app_2_trust/tests.py
+++ app_2_trust/tests.py
@@ -2,6 +2,7 @@ from otree.api import Currency as c, currency_range
 from . import pages
 from ._builtin import Bot
 from .models import Constants
+import itertools, random
 
 
 class PlayerBot(Bot):
@@ -10,15 +11,17 @@ class PlayerBot(Bot):
         if self.round_number == 1:
             yield pages.app_2_trust_intro
         if self.player.metarole == True:
-            yield pages.app_2_trust_send, dict(sent_amount = 2)
-            yield pages.app_2_trust_beliefs_sender, dict(sender_belief_if1 = True, sender_belief_if2 = True, sender_belief_shock = 2)
-            yield pages.app_2_trust_sendback, dict(sent_back_amount_if1 = True, sent_back_amount_if2 = True)
-            yield pages.app_2_trust_beliefs_receiver, dict(receiver_belief = 2, receiver_belief_shock = 2)
+            yield pages.app_2_trust_send, dict(sent_amount = random.randint(0, 2))
+            yield pages.app_2_trust_beliefs_sender, dict(sender_belief_if1 = bool(random.randint(0, 1)), sender_belief_if2 = bool(random.randint(0, 1)), sender_belief_shock = random.randint(1, 3))
+            yield pages.app_2_trust_sendback, dict(sent_back_amount_if1 = bool(random.randint(0, 1)), sent_back_amount_if2 = bool(random.randint(0, 1)))
+            yield pages.app_2_trust_beliefs_receiver, dict(receiver_belief = random.randint(0, 2), receiver_belief_shock = random.randint(1, 3))
         if self.player.metarole == False:
-            yield pages.app_2_trust_sendback, dict(sent_back_amount_if1 = True, sent_back_amount_if2 = True)
-            yield pages.app_2_trust_beliefs_receiver, dict(receiver_belief = 2, receiver_belief_shock = 1)
-            yield pages.app_2_trust_send, dict(sent_amount = 2)
-            yield pages.app_2_trust_beliefs_sender, dict(sender_belief_if1 = True, sender_belief_if2 = True, sender_belief_shock = 1)
+            yield pages.app_2_trust_sendback, dict(sent_back_amount_if1 = bool(random.randint(0, 1)), sent_back_amount_if2 = bool(random.randint(0, 1)))
+            yield pages.app_2_trust_beliefs_receiver, dict(receiver_belief = random.randint(0, 2), receiver_belief_shock = random.randint(1, 3))
+            yield pages.app_2_trust_send, dict(sent_amount = random.randint(0, 2))
+            yield pages.app_2_trust_beliefs_sender, dict(sender_belief_if1 = bool(random.randint(0, 1)), sender_belief_if2 = bool(random.randint(0, 1)), sender_belief_shock = random.randint(1, 3))
         if self.round_number == 2:
             yield pages.app_2_trust_main_results
+
+# bool(random.getrandbits(1))
 
